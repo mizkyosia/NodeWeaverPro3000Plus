039ed924b8c3cf382259630385ffbab1c9e86075
mizkyosia <mizkyosia@gmail.com>
2025-03-19 14:56:50 +0100

    Fixing DB connection

--- src/lib/db-connect.ts
+++ src/lib/db-connect.ts
@@ -1,11 +1,16 @@
-import mysql from "mysql";
+import mysql from "mysql2";
 
-export const connection = mysql.createConnection({
+const config = {
+    host: 'db',
     port: parseInt(process.env.MYSQLDB_DOCKER_PORT ?? "3306"),
     user: process.env.MYSQLDB_USER,
     password: process.env.MYSQLDB_ROOT_PASSWORD,
     database: process.env.DB_NAME
-})
+};
+
+console.log("Current config : ", config);
+
+export const connection = mysql.createConnection(config);
 
 connection.connect((e) => {
     if (e) {
